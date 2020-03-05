cee110ebf5b0da182108297636959168589406a8
allison_johnson <allison.cuttler@gmail.com>
2020-03-05 14:33:23 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,13 +16,25 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def index
+    if params[:query]
+      @students = Student.search(params[:query])
+    else
+      @students = Student.all
+    end
+  end
+
   def show
     @student = Student.find(params[:id])
   end
 
-  def index
-    @students = Student.all
-  end
+  # def search
+  #   #binding.pry
+  #   @
+  #   #How do we pass @students to index action?
+  #   #Or maybe params should get passed right to index action?
+  #   #Can one action have both a 'post' and a 'get'?
+  # end
 
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :classrooms
   resources :students
+  post 'search', to: 'students#search'
 end
