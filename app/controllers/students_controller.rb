702cdda438955039f6a6cce5f8312dfc9ea0a5cc
ricardo39985 <ricardo39985@gmail.com>
2020-03-09 09:35:24 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,10 +21,11 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.search(params[:search])
+    @students = Student.search(params[:query])
+    # byebug
   end
 
   def student_params
-    params.require(:student).permit(:name, :birthday, :hometown, :search)
+    params.require(:student).permit(:name, :birthday, :hometown, :query)
   end
 end
